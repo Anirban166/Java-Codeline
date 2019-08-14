2aaba7696b41aeb12db0e7fdb1ab3dfab9f607ad
Anirban Chetia <bloodraven166@gmail.com>
2019-08-14 23:05:14 +0530

    Reverse singly-linked and doubly-linked lists.

--- Linked Lists/Reverse.java
+++ Linked Lists/Reverse.java
@@ -1,3 +1,5 @@
+//Singly
+
 static SinglyLinkedListNode reverse(SinglyLinkedListNode head) 
 {
     if (head == null || head.next == null) 
@@ -11,6 +13,28 @@ static SinglyLinkedListNode reverse(SinglyLinkedListNode head)
     {
         next = curr.next;
         curr.next = prev; // changes arrow direction
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+//Doubly:
+
+DoublyLinkedListNode reverse(DoublyLinkedListNode head) 
+{
+    if (head == null || head.next == null) 
+    {
+        return head;
+    }
+    DoublyLinkedListNode prev = null;
+    DoublyLinkedListNode curr = head;
+    DoublyLinkedListNode next = null;
+    while (curr != null) 
+    {
+        next = curr.next;
+        curr.next = prev;
+        curr.prev = next;
         prev = curr;
         curr = next;
     }
