2ab4f9d1a108cccdfe6dd5464a07e213d472ac6f
Anirban Chetia <bloodraven166@gmail.com>
2019-08-14 22:55:55 +0530

    Node Deletion. (Linked List)

---
+++ Node_Deletion.java
@@ -0,0 +1,17 @@
+Node Delete(Node head, int position) {
+    if (head == null) {
+        return null;
+    } else if (position == 0) {
+        return head.next;
+    } else {
+        // Get Node one element before desired position 
+        Node n = head;
+        for (int i = 0; i < position - 1; i++) {
+            n = n.next;
+        }
+        // Delete Node 
+        n.next = n.next.next;
+        
+        return head;
+    }
+}
