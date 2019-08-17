3867fadd3a42f8e3d5fb18c3ab4d5dd67389217f
Anirban Chetia <bloodraven166@gmail.com>
2019-08-17 11:47:47 +0530

    ArrayList implementation of Stack.

---
+++ Stacks/ArrayList.java
@@ -0,0 +1,52 @@
+Import ArrayList from utils | LIFO ArrayList
+
+public class StackArrayList 
+{
+    public static void main(String[] args) 
+    {
+        
+        StackArrayList myStackArrayList = new StackArrayList();
+        
+//Insertions: myStackArrayList.push(10);  myStackArrayList.push(11);
+
+     System.out.println(myStackArrayList.isEmpty()); // return false if not empty.
+     System.out.println(myStackArrayList.peek()); // 11 
+     System.out.println(myStackArrayList.pop()); // 11
+     System.out.println(myStackArrayList.peek()); // 10
+     System.out.println(myStackArrayList.pop()); // 10
+    }
+
+    //ArrayList representation of the stack : specifier ArrayList<type> name;
+    public ArrayList<Integer> stackList;
+
+    //Constructor :
+    public StackArrayList() 
+    { stackList = new ArrayList<>(); }
+
+        //Add to end of list:    
+        public void push(int value) 
+        { stackList.add(value); }
+
+    //pops top element: (last inserted)
+    public int pop() 
+        {
+        if (!isEmpty()) //if stack empty
+        { 
+            int popValue = stackList.get(stackList.size() - 1);
+            stackList.remove(stackList.size() - 1);  // removes popped element
+            return popValue;
+        }
+        System.out.print("The stack is already empty!");
+        return -1;
+       }
+
+    //Empty stack check
+    public boolean isEmpty() 
+    { return stackList.isEmpty(); }
+
+   //Return top element:
+    public int peek() 
+    {
+        return stackList.get(stackList.size() - 1);
+    }
+}
