f6e9130befe328d4c827a1e9def1dfb10e62b2fa
Anirban Chetia <bloodraven166@gmail.com>
2019-08-18 11:26:07 +0530

    Implementation of Stack using LinkedLists.

--- Stacks/LinkedList_Stack.java
+++ Stacks/LinkedList_Stack.java
@@ -1,16 +1,10 @@
-package DataStructures.Stacks;
+// import package DataStructures.Stacks;
 
-/**
- * @author Varun Upadhyay (https://github.com/varunu28)
- */
+class StackOfLinkedList 
+{
 
-// An implementation of a Stack using a Linked List
-
-class StackOfLinkedList {
-
-    public static void main(String[] args) {
-
-        LinkedListStack stack = new LinkedListStack();
+    public static void main(String[] args) 
+    {   LinkedListStack stack = new LinkedListStack();
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -19,50 +13,48 @@ class StackOfLinkedList {
 
         stack.printStack();
 
-        System.out.println("Size of stack currently is: " + stack.getSize());
+        System.out.println("Size of stack currently is: " + stack.getSize()); //5
 
         stack.pop();
         stack.pop();
 
-        System.out.println("Top element of stack currently is: " + stack.peek());
+        System.out.println("Top element of stack currently is: " + stack.peek()); //5
     }
 }
 
-// A node class
-
-class Node {
+class Node // A node class
+{
     public int data;
     public Node next;
 
-    public Node(int data) {
+    public Node(int data) 
+    {
         this.data = data;
         this.next = null;
     }
 }
 
-/**
- * A class which implements a stack using a linked list
- * <p>
- * Contains all the stack methods : push, pop, printStack, isEmpty
- **/
-
-class LinkedListStack {
-
+class LinkedListStack 
+{
     Node head = null;
-
-    public void push(int x) {
+    public void push(int x) 
+    {
         Node n = new Node(x);
-        if (head == null) {
+        if (head == null) 
+        {
             head = n;
-        } else {
+        } else 
+        {
             Node temp = head;
             n.next = temp;
             head = n;
         }
     }
 
-    public void pop() {
-        if (head == null) {
+    public void pop()
+    {
+        if (head == null) 
+        {
             System.out.println("Empty stack. Nothing to pop");
         }
 
@@ -71,20 +63,26 @@ class LinkedListStack {
         System.out.println("Popped element is: " + temp.data);
     }
 
-    public int peek() {
-        if (head == null) {
+    public int peek() 
+    {
+        if (head == null)
+        {
             return -1;
         }
         return head.data;
     }
 
-    public void printStack() {
+    public void printStack() 
+    {
         Node temp = head;
         System.out.println("Stack is printed as below: ");
-        while (temp != null) {
-            if (temp.next == null) {
+        while (temp != null) 
+        {
+            if (temp.next == null) 
+            {
                 System.out.print(temp.data);
-            } else {
+            } else 
+            {
                 System.out.print(temp.data + " -> ");
             }
             temp = temp.next;
@@ -92,17 +90,21 @@ class LinkedListStack {
         System.out.println();
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty() 
+    {
         return head == null;
     }
 
-    public int getSize() {
+    public int getSize() 
+    {
         if (head == null)
             return 0;
-        else {
+        else
+        {
             int size = 1;
             Node temp = head;
-            while (temp.next != null) {
+            while (temp.next != null)
+            {
                 temp = temp.next;
                 size++;
             }
